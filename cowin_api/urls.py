c7c6849b08feda78c04387d47c19af4600932c58
visheshyadav09 <61138952+visheshyadav09@users.noreply.github.com>
2021-05-13 01:26:40 +0530

    added get calender by district and wrong url handling (#6)
    * added get calender by district and wrong url handling
    
    * changed error in urls.py

--- cowin_api/urls.py
+++ cowin_api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import *
 
+
+
 urlpatterns = [
     path('', GetToken.as_view(), name="get-token"),
-    path('get-beneficiary/', GetBeneficiaries.as_view(), name="get-beneficiary")
+    path('get-beneficiary/', GetBeneficiaries.as_view(), name="get-beneficiary"),
+    path('get-calender-by-district/<district_id>/<date>/',GetCalenderbydistrict.as_view(),name='get-calender-by-district')
 ]
